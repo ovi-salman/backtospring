dceab40e7f9579a13df2c71927b1e0c3f6e21004
cptNemo <ovvi@outlook.com>
2020-11-27 10:15:08 +0600

    Done with setup on local

---
+++ src/main/java/web/backtospring/entities/Member.java
@@ -0,0 +1,81 @@
+package web.backtospring.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Entity
+public class Member {
+
+
+    @Id
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "MEMBER_ROLES", joinColumns = {
+            @JoinColumn(name = "MEMBER_EMAIL", referencedColumnName = "email")}, inverseJoinColumns = {
+            @JoinColumn(name = "ROLE_NAME", referencedColumnName = "name") })
+    private List<Role> role;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
+
+    public Member(String email, String firstName, String lastName, String password ) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public Member() {
+    }
+}
